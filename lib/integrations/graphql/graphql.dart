0e080b8f95294d9ad30a875b89ac16be878d9aec
randomUser <earon.pabon@gmail.com>
2020-02-29 18:32:15 +0800

    Activity page viewing update

--- lib/integrations/graphql/graphql.dart
+++ lib/integrations/graphql/graphql.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 
 ValueNotifier<GraphQLClient> getClient(){
-  final HttpLink httpLink = HttpLink(uri: "http://ec2-18-139-217-149.ap-southeast-1.compute.amazonaws.com:1973/graphql");
+  final HttpLink httpLink = HttpLink(uri: "http://ec2-54-169-51-49.ap-southeast-1.compute.amazonaws.com:1973/graphql");
 
   return ValueNotifier(
       GraphQLClient(
